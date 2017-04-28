d1158c92a345cd3c16370986bebe51d7faa67236
xenia crasta <cxenia02gmail.com>
2017-04-28 13:28:03 +0530

    successfully loaded static resources

--- shoppingcart/src/main/java/net/xc/shoppingcart/controller/PageController.java
+++ shoppingcart/src/main/java/net/xc/shoppingcart/controller/PageController.java
@@ -1,16 +1,46 @@
 package net.xc.shoppingcart.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
 
-	@RequestMapping(value = {"/" , "/home" , "/index"})
+	@RequestMapping(value = { "/", "/home", "/index" })
 	public ModelAndView index() {
-		 ModelAndView mv =new ModelAndView( "page" );
-		 mv.addObject("greeting" , "welcome to spring Web MVC"); 
-		 return mv; 
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "welcome to spring Web MVC");
+		return mv;
 	}
-	}
+
+	// @RequestMapping(value="/test")
+	// public ModelAndView test(@RequestParam(value="greeting" ,
+	// required=false)String greeting){
+	// if(greeting == null){
+	// greeting = "hello there";
+	// }
+	// ModelAndView mv =new ModelAndView( "page" );
+	// mv.addObject("greeting" ,greeting);
+	// return mv;
+	// }
+
+      @RequestMapping(value="/test/{greeting}")
+      public ModelAndView test(@PathVariable("greeting" )String greeting){
+          if(greeting == null){
+                              greeting = "hello there";
+             }
+            ModelAndView mv =new ModelAndView( "page" );
+            mv.addObject("greeting" ,greeting);
+            return mv;
+         }
+}
+
+
+
+
+
+
+
+
